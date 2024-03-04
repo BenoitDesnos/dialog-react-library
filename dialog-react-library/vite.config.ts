d0ad6ed9ba37e66727b67bb61d2a094cafc0fa94
Benoît DESNOS <benoit@season-ed.com>
2024-03-05 00:33:30 +0100

    Remove index.html and vite.svg, update package.json and vite.config.ts

--- dialog-react-library/vite.config.ts
+++ dialog-react-library/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/components/index.tsx"),
+      name: "dialog-react-library",
+      fileName: "dialog-react-library",
+    },
+
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+});
